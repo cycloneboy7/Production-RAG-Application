ddcc2c1c04ee8c188a27e6ad788e28071a922406
cycloneboy7 <521akhilarjun@gmail.com>
2026-06-30 23:24:46 +0530

    Tested API and AI

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 def main():
-    print("Hello from production-rag-application!")
+    #test google generative ai model
+    model = ChatGoogleGenerativeAI(model="gemini-3.5-flash", temperature=0)
+    response = model.invoke("Setup Done")
+    print(f"Response from Google:{response}")
 
 
 if __name__ == "__main__":
