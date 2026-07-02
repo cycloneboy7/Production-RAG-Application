271f7a79e533650f60ed834a990b59d7067237ca
cycloneboy7 <521akhilarjun@gmail.com>
2026-07-02 21:19:31 +0530

    PDF and Text Loader

--- doc_loader.py
+++ doc_loader.py
@@ -7,14 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #text file loaders
-def load_text_file():
+def load_text_file(): # def PyPDFLoader(file_path: str):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".txt") as temp_file:
         temp_file.write(b"This is a sample text file for testing.")
         temp_file_path = temp_file.name
     try:
+        #loader = PyPDFLoader(temp_file_path)
         loader = TextLoader(temp_file_path)
         doc = loader.load()
-        for i in doc:
+        for i in doc: #for i,doc in enumerate(doc):
+            print(i)
             print(i.page_content)
     finally:
         os.unlink(temp_file_path)
+        
+        
+if __name__ == "__main__":
+    load_text_file()
+    #PyPDFLoader("sample.pdf")
